2d1ef7e24b1aad51c8870a6196a7f0d05beea48f
paul-murithi <paulmurithi233@gmail.com>
2023-10-08 14:07:44 +0300

    Template files for backend implementation.

---
+++ src/main/java/com/paul/backend/login/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.paul.backend.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Servlet called");
+		
+		response.sendRedirect("/TransitEase_travel_agency/Pages/Homepage/index.jsp");
+	}
+
+}
