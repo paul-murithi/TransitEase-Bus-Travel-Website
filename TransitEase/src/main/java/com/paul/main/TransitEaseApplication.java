9d7e861e43a6e43aa24da3de154affe6d052b97b
paul-murithi <paulmurithi233@gmail.com>
2023-11-04 22:16:36 +0300

    Spring starter files

---
+++ TransitEase/src/main/java/com/paul/main/TransitEaseApplication.java
@@ -0,0 +1,13 @@
+package com.paul.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitEaseApplication.class, args);
+	}
+
+}
---
+++ TransitEase/src/test/java/com/paul/main/TransitEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
