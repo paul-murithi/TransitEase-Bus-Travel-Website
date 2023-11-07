5deeea1be86deade9a23ba7e93ff5b3d69bca11d
paul-murithi <paulmurithi233@gmail.com>
2023-11-07 18:20:32 +0300

    Spring boot commit 003 Initialized new local folder Added new updated code

---
+++ TransitEase_project/src/main/java/com/paul/demo/TransitEaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paul.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitEaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitEaseProjectApplication.class, args);
+	}
+
+}
