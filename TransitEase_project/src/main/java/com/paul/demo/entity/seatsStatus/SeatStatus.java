84be899f107bb187a2e095331d623628cd1abb9f
Paul Murithi Kirera <114237403+paul-murithi@users.noreply.github.com>
2026-03-06 16:57:20 +0300

    Delete TransitEase_project directory

--- TransitEase_project/src/main/java/com/paul/demo/entity/seatsStatus/SeatStatus.java
+++
@@ -1,6 +0,0 @@
-package com.paul.demo.entity.seatsStatus;
-
-public enum SeatStatus {
-    AVAILABLE,
-    BOOKED
-}
--- TransitEase_project/src/main/java/com/paul/demo/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paul.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.paul.demo.entity.Bookings;
-
-public interface BookingRepository extends JpaRepository<Bookings, Long> {
-
-}
